af7a7a7e02fbc9021e84e6c20f6daea8bac90b5a
Rasyidaa15 <rasyidamessy@gmail.com>
2026-06-26 18:40:51 +0700

    Update Supplier.js

--- models/Supplier.js
+++ models/Supplier.js
@@ -2,12 +2,32 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Supplier = sequelize.define('Supplier', {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  contact: { type: DataTypes.STRING },
-  address: { type: DataTypes.TEXT },
-  createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-  updatedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  contact: {
+    type: DataTypes.STRING
+  },
+  address: {
+    type: DataTypes.TEXT
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'suppliers',
+  timestamps: true
 });
 
 module.exports = Supplier;
